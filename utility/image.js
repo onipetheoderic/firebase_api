4e4dbeb5f6a073fdfc7c295f219cfdce902f298b
Theoderic <onipetheoderic@gmail.com>
2020-05-07 17:52:29 +0100

    all

--- utility/image.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-
-exports.imagePlacerAndNamer = (req, res, the_file)=>{
-    let file_name = Date.now()+ the_file.name
-    the_file.mv('Views/public/uploads/' + file_name, function(err) {
-   });
-    return file_name
-}
